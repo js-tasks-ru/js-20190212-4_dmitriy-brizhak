1990aaac7a626e310e99ac059ffbf5489a8d46ba
Dmitrii Brizhak <flyu.gyu@yandex.ru>
2019-03-21 22:32:03 +0300

    Решение 1.2

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -12,6 +12,10 @@ function print (text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid (name) {
+  if (!name) return false;
+  if (name.length < 4) return false;
+  if (!(name.indexOf(" ") === -1)) return false;
+  return true;
 }
 
 function sayHello () {
@@ -25,4 +29,3 @@ function sayHello () {
 }
 
 sayHello();
-
