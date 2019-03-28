a1e5058fdb97c0a5c27dd41d4147f900b9112588
Dmitrii Brizhak <flyu.gyu@yandex.ru>
2019-03-28 11:57:07 +0300

    0-1 1-3 2-2

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -5,7 +5,13 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
-function getMinMax(str) {
-
-}
-
+ function getMinMax(str) {
+     const result = str.match( /[\d.-]+/ig ).reduce(function(minMax, current, i){
+         current = +current;
+         if (i == 0) return minMax = {min:current, max:current};
+         if (minMax.min > current) minMax.min = current;
+         if (minMax.max < current) minMax.max = current;
+         return minMax;
+     },{});
+     return result;
+ }
--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -4,5 +4,26 @@
  * @param {*} value - значение, которе ищем
  * @returns {Object}
  */
-function find (obj, value) {
-}
+ function find (obj, value) {
+   let arr = [];
+   for (var key in obj) {
+     if (typeof obj[key] === "object") {
+         if (find (obj[key], value) != null) arr.push([key, find (obj[key], value)]);
+         console.log('Рекурсия ',arr);
+     };
+     if (obj[key] === value) {
+         arr.push(key);
+
+     }
+   }
+   if (arr.length === 0) return null;
+   arr = arr.map(function(value){
+       if (typeof value === "object")  {
+           console.log("Сооденяем ",value);
+           return value.join(".")
+       };
+       return value;
+   })
+   if (arr.length === 1) return arr[0];
+   return arr;
+ }
