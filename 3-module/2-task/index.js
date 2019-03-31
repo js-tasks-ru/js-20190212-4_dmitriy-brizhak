19cf52f34bc64979c84621dc6258db5e57db33af
Dmitrii Brizhak <flyu.gyu@yandex.ru>
2019-03-31 08:52:29 +0300

    3-2

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,6 +4,26 @@ let calendar = {
 };
 
 calendar[Symbol.iterator] = function () {
+let currentDate = new Date(this.from);
+let lastDate = new Date(this.to);
+
+  // метод должен вернуть объект с методом next()
+  return {
+    next() {
+        if (currentDate < lastDate) {
+             currentDate.setDate(currentDate.getDate() + 1);
+             let date = (currentDate.getDate() < 10) ? '0'+currentDate.getDate() : currentDate.getDate();
+             date = (currentDate.getDay() === 0 || currentDate.getDay() === 6) ? `[${date}]` : `${date}`;
+            return {
+            done: false,
+            value: date
+            };
+        } else {
+            return {
+            done: true
+            };        
+        }
+    }
+
+  }
 };
-
-
