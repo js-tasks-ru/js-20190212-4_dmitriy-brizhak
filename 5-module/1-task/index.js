8b1a7c73b38325300f5b5a3a5159e47712f80d1d
Dmitrii Brizhak <flyu.gyu@yandex.ru>
2019-04-01 16:03:47 +0300

    5-1

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -5,4 +5,28 @@
  * @param {Element} table
  */
 function highlight(table) {
+    const thead = table.rows[0].cells;
+    const tr = table.rows;
+    //debugger;
+    let age, gender, status;
+    for (let i = 0; i < thead.length; i++) {
+        if (thead[i].innerHTML === 'Age') age = i;
+        if (thead[i].innerHTML === 'Gender') gender = i;
+        if (thead[i].innerHTML === 'Status') status = i;
+    }
+
+    for (let i = 1; i < tr.length; i++){
+        if (tr[i].cells[status].dataset.available) {
+            tr[i].classList.add( (tr[i].cells[status].dataset.available === 'true') ? 'available' : 'unavailable');
+        } else {
+            tr[i].setAttribute('hidden','hidden');
+        };
+        tr[i].classList.add( (tr[i].cells[gender].innerHTML === 'm') ? 'male' : 'female');
+        if (tr[i].cells[age].innerHTML < 18) {
+            tr[i].setAttribute('style',"text-decoration: line-through");
+        }
+    }
 }
+
+
+
