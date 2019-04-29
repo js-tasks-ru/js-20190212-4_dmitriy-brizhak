b7b13ed5f476bb5d776cc1074f18a9b30f482855
Dmitrii Brizhak <flyu.gyu@yandex.ru>
2019-04-29 11:20:29 +0300

    12-1

--- 12-module/1-task/webpack.config.js
+++ 12-module/1-task/webpack.config.js
@@ -4,3 +4,23 @@ module.exports = {
      * Для ссылки на текущую папку используйте встроенную переменную __dirname
      */
 };
+
+module.exports = {
+    entry: __dirname + '/modules/app.js',
+    output: {
+      path: __dirname,
+      filename: 'index.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['babel-preset-env']
+            }
+          }
+        }]
+    }
+  };
